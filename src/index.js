0b6792cb186d582bf64fdc5f74f2a65379917f48
stephnna <stephennnabuike@gmail.com>
2022-07-06 23:36:46 +0100

    Implemented to do list

--- src/index.js
+++ src/index.js
@@ -1,13 +1,38 @@
-import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+const toDos = [
+  {
+    description: 'Attend Morning Session',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'Complete Peer-to-peer code review',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Complete to do project',
+    completed: false,
+    index: 2,
+  },
+];
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+const myToDos = document.getElementById('to-dos');
+// button = document.getElementById('add-btn');
 
-  return element;
-}
+const displayBooks = (todo) => {
+  let toDoContainer = '';
+  todo.forEach((arrayItem) => {
+    const toDoContent = `<div class="to-do-container">
+    <label>
+    <input type="checkbox">${arrayItem.description}    
+    </label>
+    <div class="three-dots"></div>      
+  </div><hr>`;
+    toDoContainer += toDoContent;
+  });
+  myToDos.innerHTML = toDoContainer;
+};
 
-document.body.appendChild(component());
+displayBooks(toDos);
