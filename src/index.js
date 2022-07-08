e45e041d0243c40a21fd9c0321d4dabacb2d77c8
stephnna <stephennnabuike@gmail.com>
2022-07-08 10:45:37 +0100

    Update css, js and html files

--- src/index.js
+++ src/index.js
@@ -1,38 +1,9 @@
 import './style.css';
-
-const toDos = [
-  {
-    description: 'Attend Morning Session',
-    completed: false,
-    index: 0,
-  },
-  {
-    description: 'Complete Peer-to-peer code review',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'Complete to do project',
-    completed: false,
-    index: 2,
-  },
-];
-
-const myToDos = document.getElementById('to-dos');
-// button = document.getElementById('add-btn');
-
-const displayBooks = (todo) => {
-  let toDoContainer = '';
-  todo.forEach((arrayItem) => {
-    const toDoContent = `<div class="to-do-container">
-    <label>
-    <input type="checkbox">${arrayItem.description}    
-    </label>
-    <div class="three-dots"></div>      
-  </div><hr>`;
-    toDoContainer += toDoContent;
-  });
-  myToDos.innerHTML = toDoContainer;
-};
-
-displayBooks(toDos);
+import TodosClass from './modules/todo_class.js';
+// create an instant of a class
+const newTodos = new TodosClass();
+// call class methods(i.e functions inside the class)
+newTodos.getList();
+newTodos.addOnClick();
+newTodos.editTask();
+newTodos.deleteList();
